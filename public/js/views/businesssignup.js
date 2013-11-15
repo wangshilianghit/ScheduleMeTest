bc051938257e1ac0dd762dd6c80e672ee561fccd
Peter <wangshilianghit@gmail.com>
2013-11-15 01:38:05 -0500

    Change businesssingup.js

--- public/js/views/businesssignup.js
+++ public/js/views/businesssignup.js
@@ -11,7 +11,8 @@ window.BusinessSignupView = Backbone.View.extend({
 
     events:{
         "click #employeeSignup":"employeeSignupClicked",
-        "click #customerSignup":"customerSignupClicked"
+        "click #customerSignup":"customerSignupClicked",
+        "click #signupButton":"signupBusinessClicked"
     },
 
     render:function () {
@@ -23,11 +24,40 @@ window.BusinessSignupView = Backbone.View.extend({
         app.employeeSignupView.render();
     },
 
-    customerSignupClicked: function (){
-        app.customerSignupView.render();
+    signupBusinessClicked: function (){
+        app.businessSignupView.render();
+    },
+
+    signupBusinessClicked:function (event) {
+        event.preventDefault(); // Don't let this button submit the form
+        $('.alert-error').hide(); // Hide any errors on a new submit
+        var url = '../signup_business';
+        console.log('signing up a business ');
+        var formValues = {
+            firstName: $('#inputFirstName').val(),
+            lastName: $('#inputLastName').val(),
+            email: $('#inputEmail').val(),
+            password: $('#inputPassword').val()
+        };
+
+        $.ajax({
+            url:url,
+            type:'POST',
+            dataType:"json",
+            data: formValues,
+            success:function (data) {
+
+                console.log(["Business Signup request details: ", data]);
+
+                if(data.error) { // If there is an error, show the error messages
+                    $('.alert-error').text(data.error.text).show();
+                }
+                else { // If not, send them back to the home page
+                    Backbone.history.navigate("/login");
+                }
+            }
+        });
     }
-
-
 
 
 
