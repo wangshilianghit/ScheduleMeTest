61011b4762fe274517581e68842ae19f28eda93f
RSS2013 <rickysharma05@gmail.com>
2013-12-06 11:00:56 -0500

    applied the extract method on main.js (recommended by Prof/TA from our last feedback.) Please note use the routeInitialViewCreator.make method to define route actions.

--- public/js/utils.js
+++ public/js/utils.js
@@ -19,3 +19,27 @@ window.templateLoader = {
     }
 
 };
+
+window.routeInitialViewCreator = {
+
+    make: function(that,name,model,where){
+        var upper = name;
+
+        var lower = name.charAt(0).toLowerCase() + name.slice(1);
+            console.log(upper);
+        console.log(lower);
+       /* this.businessServiceSettingsView = new BusinessServiceSettingsView({model:this.userModel});
+        this.businessServiceSettingsView.render();
+
+
+        $("#subcontent").html(this.businessServiceSettingsView.el);
+        */
+        eval("that." + lower + "= new " + upper + "({model:" + "that." + model + "})");
+        eval("that." + lower + ".render()");
+
+        eval("$(" +"\"" + where + "\")" + ".html(that." + lower + ".el)");
+
+    }
+
+
+};
