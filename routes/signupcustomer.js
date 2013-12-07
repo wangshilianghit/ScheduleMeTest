0174536026f99fecca11e5996c1dd56bad0eb9a0
RSS2013 <rickysharma05@gmail.com>
2013-12-06 20:08:04 -0500

    basic employee table created.

---
+++ routes/signupcustomer.js
@@ -0,0 +1,29 @@
+
+/*
+ * GET home page.
+ */
+var path = require('path');
+var User = require('../models/user.js');
+
+exports.signupcustomer = function (req, res) {
+    requestASJSON = JSON.parse(JSON.stringify(req.body));
+    console.log(requestASJSON);
+    email = requestASJSON.email;
+    password = requestASJSON.password;
+    firstName = requestASJSON.firstName;
+    lastName = requestASJSON.lastName;
+
+    <!-- Should this have typeAccount: "customer"? -->
+    var newUser = new User({email: email, password: password, firstName: firstName, lastName: lastName});
+
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+            res.send(500, {error: "DB error"});
+        }
+        else{
+            res.send({message:'success'});
+        }
+
+    });
+};
